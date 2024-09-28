dc6c7ccd442f135f9dd356614323e841722c80d7
safdarAwaN786 <safdarawan2003@gmail.com>
2024-09-28 23:50:48 +0500

    issues resolved

--- backend/src/db/connectdb.js
+++ backend/src/db/connectdb.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 // * Updated MongoDB Atlas SRV connection link
-// const dbConnect = 'mongodb+srv://safdar:sJQVITzTeIfUteRl@new-feat.ey8gy4c.mongodb.net/New-Feat';
-const dbConnect = 'mongodb://localhost:27017/New-Feat';
+const dbConnect = 'mongodb+srv://safdar:sJQVITzTeIfUteRl@new-feat.ey8gy4c.mongodb.net/New-Feat';
+// const dbConnect = 'mongodb://localhost:27017/New-Feat';
 
 // * MongoDB Atlas connection using mongoose
 mongoose.connect(dbConnect)
